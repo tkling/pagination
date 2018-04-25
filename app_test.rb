1f0b6d18ae4140fcc5a0921392d44a87cdabcbac
tkling <tyling@gmail.com>
2018-04-25 01:00:38 -0400

    assert how name ranges look

--- app_test.rb
+++ app_test.rb
@@ -111,4 +111,19 @@ class HelloTest < Test::Unit::TestCase
     assert_equal 10, json_response.first['id']
     assert_equal 6, json_response.last['id']
   end
+
+  # I wanted to include this to show how my name ranges work.
+  # It's silly, but it's technically a range :) (not sorted by id)
+  def test_inclusive_bounded_range_name_5_to_6_max_2
+    header 'Range', 'name my-app-1..my-app-4; max=4'
+
+    get '/'
+    json_response = JSON.parse(last_response.body)
+
+    assert last_response.ok?
+    assert_equal Array, json_response.class
+    assert_equal 4, json_response.count
+    assert_equal 1, json_response.first['id']
+    assert_equal 101, json_response.last['id']
+  end
 end
